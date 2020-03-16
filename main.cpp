f74732416d3ba453bc3e5b4f917c5e7431992952
AndrewSmetanin <smtnin@gmail.com>
2020-03-16 18:57:58 +0400

    fix travis2

--- main.cpp
+++ main.cpp
@@ -4,11 +4,6 @@
 using namespace std;
 int main()
 {
-<<<<<<< HEAD
-=======
-    //auto logger = spdlog::stdout_logger_mt("console");
-    //logger->info("version {} was started", version());
->>>>>>> 4d7be57a5fc8499ed10b561e442e9780dfe0591b
     cout<<"build "<<version()<<endl;
     cout<<"Hello, World!"<<endl;
     return 0;
